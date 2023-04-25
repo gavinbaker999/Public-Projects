6e38ab5b50cadaf09c66d929ca0cdc8cedb425cf
gavinbaker999 <endhousesoftware999@gmail.com>
2023-04-25 16:42:15 +0100

    Updates

--- dotNET_Projects/yokogawa/FileWordCount/Program.cs
+++ dotNET_Projects/yokogawa/FileWordCount/Program.cs
@@ -5,16 +5,28 @@ using Systems.Collections;
 namespace FileWordCount {
     class Program {
         static void Main(string[] args) {
+
+            // set default values
+            int directorySearchOption = SearchOption.AllDirectories;
+
+            // process the command line arguments
+            if (args.length == 0) {
+                Console.WriteLine("FileWordCount <directory> [-s]");
+                return;
+            }
             if (args.length == 0) {
                 Console.WriteLine("FileWordCount <directory> [-s]");
                 return;
             }
 
+            // create a master list of fileInfoBlocks that hold the word stats for each file
             List<fileInfoBlock> fieInfoBlocks = new List<fileInfoBlock>();
 
-            string[] files = Directory.GetFiles(args[0]);
+            // get all the files in the current directory
+            string[] files = Directory.GetFiles(args[0],directorySearchOption);
 
             foreach (string file in files) {
+                // create a fileinfoBlock to contain the word stats for a file
                 fileInfoBlock fib = new fileInfoBlock(file);
 
                 using (StreamReader sr = new StreamReader(file)) {
@@ -27,9 +39,11 @@ namespace FileWordCount {
                     }
                 }
 
+                // add the fileInfoBlock to the master list
                 fileInfoBlocks.Add(fib);
             }
 
+            // display the word stats for each fileInfoBlock
             foreach (fileInfoBlock fib in fileInfoBlocks) {
                 fib.printStats();
             }
@@ -38,7 +52,7 @@ namespace FileWordCount {
 
     public class fileInfoBlock {
         private string pathFileName {get;set;} = "";
-        private Dictionary <string,int> wordStats {get;set;} = new Dictionary<string,int>();
+        private Dictionary <string,int> wordStats = new Dictionary<string,int>();
 
         public fileInfoBlock (string pathFileName) {
             this.pathFileName = pathFileName;
@@ -46,6 +60,7 @@ namespace FileWordCount {
         ~fileInfoBlock() {
             wordStats.Clear();
         }
+        // add a word to the wordStats dictionary if it does not exist or increase the count if it does
         public void addWord(string word) {
             word = word.Trim().ToLower();
             if (wordStats.ContainsKey(word)) {
@@ -54,6 +69,7 @@ namespace FileWordCount {
                 wordStats.Add(word,1);
             }
         }
+        // display the word stats for this fileInfoBlock
         public void printStats() {
             Console.WriteLine("File: {0}", pathFileName);
             foreach (KeyValuePair<string,int> kvp in wordStats) {
